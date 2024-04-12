f7cd2d2f53ee26a34e43a50af8b908d60d3e4904
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-04-12 00:11:43 -0700

    Updates

---
+++ precache-manifest.d28c947f9bc34396652aab6e6593a5f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a63ad2f4663fde4ef65b6312b648a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6e94a6a2c5c8c9ef01",
+    "url": "/static/css/main.98a1a592.chunk.css"
+  },
+  {
+    "revision": "a21a0d73b6f2f6bff969",
+    "url": "/static/js/2.c78a0834.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/static/js/2.c78a0834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f2e3089815cb9824fa",
+    "url": "/static/js/3.967eaa51.chunk.js"
+  },
+  {
+    "revision": "1a6e94a6a2c5c8c9ef01",
+    "url": "/static/js/main.868f7d30.chunk.js"
+  },
+  {
+    "revision": "a716dcc4bd1685425591",
+    "url": "/static/js/runtime-main.18e9bcbe.js"
+  },
+  {
+    "revision": "a84a1a24f005eeaf1a438ff5cdf78290",
+    "url": "/static/media/BAICD_BannerImage.a84a1a24.png"
+  }
+]);
