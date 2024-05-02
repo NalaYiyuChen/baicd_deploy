eca6c225103e78f2194697228a7a61b1e56e33a2
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-05-02 06:03:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14af948c7aa7e4691fff96edb3d70c60.js"
+  "/precache-manifest.bee2d7fdda5449710d121d2498959dc7.js"
 );
 
 self.addEventListener('message', (event) => {
