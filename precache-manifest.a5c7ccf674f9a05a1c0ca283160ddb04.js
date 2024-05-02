aadfd5dd897f4b987ad2bf6ce9459e9f9a4f46a8
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-05-01 22:14:53 -0700

    Updates

---
+++ precache-manifest.a5c7ccf674f9a05a1c0ca283160ddb04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fe72b5f6b76752e7fb6c359422729d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41641288289fa636dd0f",
+    "url": "/static/css/main.98a1a592.chunk.css"
+  },
+  {
+    "revision": "a241ff749978b1996859",
+    "url": "/static/js/2.67b2ab73.chunk.js"
+  },
+  {
+    "revision": "c4bd734360e03760b4860e05f599d6fb",
+    "url": "/static/js/2.67b2ab73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cd7ec75096e98b757d",
+    "url": "/static/js/3.38ea3564.chunk.js"
+  },
+  {
+    "revision": "41641288289fa636dd0f",
+    "url": "/static/js/main.95f28272.chunk.js"
+  },
+  {
+    "revision": "11333d09edf4a694bb18",
+    "url": "/static/js/runtime-main.cd343ca3.js"
+  },
+  {
+    "revision": "a84a1a24f005eeaf1a438ff5cdf78290",
+    "url": "/static/media/BAICD_BannerImage.a84a1a24.png"
+  },
+  {
+    "revision": "affa6c52f573d72f2e280bfb396812d9",
+    "url": "/static/media/baicd_logo.affa6c52.png"
+  }
+]);
